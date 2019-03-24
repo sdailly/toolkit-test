1f2eae015063a63370dbf51db9b07bd76790d76a
Stéphane Dailly <daillystephane@gmail.com>
2019-03-24 10:36:56 +0100

    delete stories

--- packages/toolkit-button/src/Button.stories.js
+++
@@ -1,9 +0,0 @@
-import { storiesOf } from '@storybook/vue';
-
-import MyButton from './Button.vue';
-
-storiesOf('Components|Button', module)
-  .add('Default', () => ({
-    components: { MyButton },
-    template: '<MyButton></MyButton>'
-  }))
